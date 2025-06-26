b88b6e50677ec5e3948adfbb0643d483969cd163
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-06-26 13:06:46 +0200

    added data configs

---
+++ data/config.py
@@ -0,0 +1,7 @@
+
+# data_dir = r"C:\Users\vorreuth\Documents\workspace\living_analysis\data"
+data_dir = r"C:\Users\vorreuth\ownCloud\Hyperscanning_Review_v2\derivatives"
+file = 'screening results.xlsx'
+
+# plots and visuals
+ColorMap = ['#00928f', '#00567c', '#6d90a0', '#38b6c0', '#728128','#bad23c', '#81c5cb', '#19bdff','#bcbec0',]
