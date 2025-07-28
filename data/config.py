51b08ce6cb9436cd43850573bfd9b4ee1dab4dd0
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-07-28 17:38:09 +0200

    added env file func

--- data/config.py
+++ data/config.py
@@ -1,6 +1,7 @@
-# data_dir = r"C:\Users\vorreuth\Documents\workspace\living_analysis\data"
-data_dir = r"C:\Users\vorreuth\ownCloud\Hyperscanning_Review_v2\derivatives"
-file = 'final screening results.xlsx'
+from dotenv import load_dotenv
+load_dotenv()
+data_dir = Path(os.getenv('data_dir'))
+file = Path(os.getenv('file'))
 
 # plots and visuals
 # ColorMap = ['#00928f', '#00567c', '#6d90a0', '#38b6c0', '#728128', '#bad23c', '#81c5cb', '#19bdff', '#bcbec0', ]
