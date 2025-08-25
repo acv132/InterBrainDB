36e1ffd2ea9c36440072ae70d0773e4f67250672
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-08-25 11:57:12 +0200

    removed src. import

--- src/pages/3_Imprint_&_Privacy_Policy.py
+++ src/pages/3_Imprint_&_Privacy_Policy.py
@@ -2,7 +2,7 @@
 # 📦 Imports & Setup
 # ========================
 import streamlit as st
-from src.utils.app_utils import render_fraunhofer_impressum, render_fraunhofer_privacy_policy, set_mypage_config
+from utils.app_utils import render_fraunhofer_impressum, render_fraunhofer_privacy_policy, set_mypage_config
 
 # ========================
 # 💅 UI Configuration
