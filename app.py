fd0bd030ed4b55cdd500d8870bc5197a7d60e332
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-09-19 10:54:09 +0200

    moved app.py to outermost folder (required)

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+pg = st.navigation([st.Page("src/pages/1_Welcome.py"),
+                    st.Page("src/pages/2_Data.py"),
+                    st.Page("src/pages/3_Submit_New_Article.py"),
+                    st.Page("src/pages/4_Imprint_&_Privacy_Policy.py"),
+                    ])
+pg.run()
