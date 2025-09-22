47e21b5ac0881a7b797d12972ffd9dc3ae91a3a8
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-09-22 11:47:20 +0200

    renamed file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([st.Page("src/pages/1_Welcome.py"),
-                    st.Page("src/pages/2_Data.py"),
+                    st.Page("src/pages/2_Database.py"),
                     st.Page("src/pages/3_Submit_New_Article.py"),
                     st.Page("src/pages/4_Imprint_&_Privacy_Policy.py"),
                     ])
