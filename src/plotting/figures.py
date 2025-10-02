8b45ea318bebd9e723f41b20e7e3403411f42577
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2025-10-02 11:03:38 +0200

    fix: interaction figure ignores ns study entries

--- src/plotting/figures.py
+++ src/plotting/figures.py
@@ -161,12 +161,12 @@ def generate_interaction_figure(df, tab, combine_modalities=False):
     prefix = "_"
     df_orig = df.copy()
     # check if df contains any non-default entries and skip those rows when generating the figure with a warning
-    non_default_rows = df_orig[
-        ~df_orig['interaction scenario'].apply(lambda x: all(s in default_scenario_order for s in ensure_list(x))) |
-        ~df_orig['interaction manipulation'].apply(
+    non_default_rows = df[
+        ~df['interaction scenario'].apply(lambda x: all(s in default_scenario_order for s in ensure_list(x))) |
+        ~df['interaction manipulation'].apply(
             lambda x: all(m in default_manipulation_order for m in ensure_list(x))
             ) |
-        ~df_orig['measurement modality'].apply(
+        ~df['measurement modality'].apply(
             lambda x: all(m in default_modalities_order for m in ensure_list(x))
             )
         ]
@@ -175,7 +175,7 @@ def generate_interaction_figure(df, tab, combine_modalities=False):
             f"⚠️ Some studies contain non-default entries in interaction scenario, manipulation, or modality. "
             f"These studies are skipped in the figure generation. Please check the data table for details."
             )
-        df_orig = df_orig.drop(non_default_rows.index)
+        df = df.drop(non_default_rows.index)
 
     df = decode_one_hot(df, prefix)
     number_studies = len(df)
